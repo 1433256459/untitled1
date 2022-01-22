2407899eca2ede5d53c032e76f524015a9d04dc9
ylp <143>
2022-01-22 12:23:53 +0800

    Initial commit

---
+++ src/main/java/com/ylp/test.java
@@ -0,0 +1,12 @@
+package com.ylp;
+
+/**
+ * @author ylp
+ * @Description
+ * @create 2022-01-22 12:01
+ */
+public class test {
+    public static void main(String[] args) {
+        System.out.println("提交");
+    }
+}
